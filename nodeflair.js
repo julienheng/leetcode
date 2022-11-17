957f2b57315c255a47346632b83fad05f7c9f02c
julien heng <hengjchan@gmail.com>
2022-11-17 15:29:20 +0800

    nodeflair coding problem

--- nodeflair.js
+++ nodeflair.js
@@ -1,24 +1,14 @@
-// Write a function that
-
-// Takes in an array ["A", "B", "C"]
+// Write a function that takes in an array ["A", "B", "C"]
 // Return the string "A says hi to B and C. B says hi to A and C. C says hi to A and B."
 
 const sayHi = function (arr) {
-  let str = "";
-
-  for (let i = 0; i < arr.length; i++) {
-    let first = `${arr[i]} says hi to `;
-    let second;
-
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[i] !== arr[j]) {
-        second = `${arr[j++]} and ${arr[j]}. `;
-      }
-    }
-
-    str += first.concat(second);
+  const result = [];
+  for (const i in arr) {
+    const tempArr = arr.map((x) => x);
+    tempArr.splice(i, 1);
+    result.push(`${arr[i]} says hi to ${tempArr.join(" and ")}.`);
   }
-  return str;
+  return result.join(" ");
 };
 
 console.log(sayHi(["A", "B", "C"]));
