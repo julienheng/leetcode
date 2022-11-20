25d5619ae3c74f8a42ecd08ab4dc3cc8abc5ae0b
julien heng <hengjchan@gmail.com>
2022-11-20 19:15:24 +0800

    updated

--- nodeflair.js
+++ nodeflair.js
@@ -3,7 +3,7 @@
 
 const sayHi = function (arr) {
   const result = [];
-  for (const i in arr) {
+  for (let i in arr) {
     const tempArr = arr.map((x) => x);
     tempArr.splice(i, 1);
     result.push(`${arr[i]} says hi to ${tempArr.join(" and ")}.`);
